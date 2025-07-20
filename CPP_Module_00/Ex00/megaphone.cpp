b1de27ad9023f54ca508eeb501269abfa979998e
Anas-Benchrifa <anas.benchrifa69@gmail.com>
2025-07-20 06:18:56 +0100

    Good Trip

---
+++ CPP_Module_00/Ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aben-chr <aben-chr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 01:57:29 by aben-chr          #+#    #+#             */
+/*   Updated: 2025/06/12 17:52:21 by aben-chr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int index = 1; index < argc; ++index)
+		{
+			for (int to_upper = 0; argv[index][to_upper] != '\0'; to_upper++)
+				argv[index][to_upper] = toupper(argv[index][to_upper]);
+			std::cout << argv[index];
+			if (argv[index] != NULL)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+}
